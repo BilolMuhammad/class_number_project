7492716837bc07a54a92b5c0c8d543d740d3771d
Bilol <mamatqulov@gmail.com>
2023-01-28 17:36:05 +0500

    number.py

--- number.py
+++ number.py
@@ -2,14 +2,13 @@ class Number:
     def __init__(self, value: int):
         self.value = value
 
-
     def get_number(self):
         """
         Returns the number.
 
         returns: int
         """
-        pass
+        return self.value
 
     def is_odd(self):
         """
@@ -18,7 +17,7 @@ class Number:
         returns: bool
 
         """
-        pass
+        return self.value % 2 == 1
 
     def is_even(self):
         """
@@ -26,7 +25,7 @@ class Number:
 
         returns: bool
         """
-        pass
+        return self.value % 2 == 0
 
     def is_prime(self):
         """
@@ -34,7 +33,14 @@ class Number:
 
         returns: bool
         """
-        pass
+        count = 0
+        for num in range(2, self.value):
+            if self.value % num == 0:
+                count += 1
+            if count == 0:
+                return True
+            else:
+                return False
 
     def get_divisors(self):
         """
@@ -42,7 +48,10 @@ class Number:
 
         returns: list
         """
-        pass
+        list = []
+        for n in range(1, self.value+1):
+            list.append(n)
+        return list
 
     def get_length(self):
         """
@@ -50,7 +59,7 @@ class Number:
 
         returns: int
         """
-        pass
+        return len(str(self.value))
 
     def get_sum(self):
         """
@@ -58,7 +67,12 @@ class Number:
 
         returns: int
         """
-        pass
+        num_value = self.value
+        sum = 0
+        while num_value:
+            sum += num_value % 10
+            num_value //= 10
+        return sum
 
     def get_reverse(self):
         """
@@ -66,7 +80,13 @@ class Number:
 
         returns: int
         """
-        pass
+        value = self.value
+        rev = 0
+        while value:
+            dig = value % 10
+            rev = rev*10+dig
+            value //= 10
+        return rev
 
     def is_palindrome(self):
         """
@@ -82,7 +102,12 @@ class Number:
 
         returns: list
         """
-        pass
+        lis = []
+        dig = self.value
+        while dig:
+            lis.append(dig % 10)
+            dig //= 10
+        return lis
 
     def get_max(self):
         """
@@ -90,7 +115,17 @@ class Number:
 
         returns: int
         """
-        pass
+        value = self.value
+        lis = []
+        while value:
+            dig = value % 10
+            lis.append(dig)
+            value //= 10
+        max = lis[0]
+        for n in range(1, len(lis)):
+            if lis[n] > max:
+                max = lis[n]
+        return max
 
     def get_min(self):
         """
@@ -98,7 +133,17 @@ class Number:
 
         returns: int
         """
-        pass
+        value = self.value
+        list = []
+        while value:
+            dig = value % 10
+            list.append(dig)
+            value //= 10
+        min = list[0]
+        for n in range(1, len(list)):
+            if list[n] < min:
+                min = list[n]
+        return min
 
     def get_average(self):
         """
@@ -106,7 +151,13 @@ class Number:
 
         returns: float
         """
-        pass
+        sum = 0
+        value = self.value
+        while value:
+            num = value % 10
+            sum += num
+            value //= 10
+        return sum//(self.value)
 
     def get_median(self):
         """
@@ -131,7 +182,7 @@ class Number:
         returns: dict
         """
         pass
-    
+
 
 # Create a new instance of Number
 number = Number(3)
